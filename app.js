2ba9ca1d5ce3ec7a06aab8ac42f42a96ba2ed20a
Vinicius Raio <raiovini@outlook.com>
2025-03-10 10:13:02 -0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+Alert('Boas Vindas ao Jogo do Raiozin');
+let = NumeroSecreto = 1944;
+let = Chute = prompt ('Escolha um numero entre 1 e 1945')
+
+if(NumeroSecreto == Chute) {
+  console.log('Isso aí, seu fudido! Você acertou!(1944)');
+}
